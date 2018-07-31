d11b060ab68b10d08209ff162182647c3c67281b
PranavDaSav <41297023+PranavDaSav@users.noreply.github.com>
2018-07-31 08:43:57 -0500

    Javascript added

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function Messages()  {
+  // make  alist of messages
+  // figure out how to access elements from the list
+  // how to generate random number
+  // how to get random element from list js
+  // remember to get snowy
+var messages = [
+  'Have a great day',
+  'You are amazing',
+  'You have an amazing potential inside of you',
+  'You are great',
+  'Don\'t judge a book by it\'s cover, if you get what I am saying',
+]
+
+alert(messages)
+
+}
+function introduceMe() {
+   alert( 'My name is Pranav Bhookya')
+}
